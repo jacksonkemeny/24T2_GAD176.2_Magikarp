be462a6e253e3f81361e3892822bff4567d73a46
N1ck5tar <159391877+N1ck5tar@users.noreply.github.com>
2024-07-17 17:56:12 +1000

    initial commit

---
+++ Assets/Scripts/Moving Objects/BaseMovingObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMovingObject : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("This is the BaseMovingObject script"); // debug what this script is
+    }
+
+    void Update()
+    {
+        
+    }
+}
