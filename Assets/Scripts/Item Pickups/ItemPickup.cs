797eeb6b297e15868d667b81ec0e634cee070eca
TaylahFrederickSAE <1032576@student.sae.edu.au>
2024-08-02 13:07:55 +1000

    Created Item Pickup Script, added player character & movement.

--- Assets/Scripts/Item Pickups/ItemPickup.cs
+++ Assets/Scripts/Item Pickups/ItemPickup.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace GAD176.ItemPickups
+    {
 public class ItemPickup : MonoBehaviour
 {
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+}
+
 }
