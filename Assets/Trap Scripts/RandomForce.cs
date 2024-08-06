9705830fd4e7b8f33082e57359e121a84c7b3679
N1ck5tar <159391877+N1ck5tar@users.noreply.github.com>
2024-08-06 17:59:28 +1000

    - Added swinging ball and chain - Added random force script for swinging ball

---
+++ Assets/Trap Scripts/RandomForce.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SAE_24T2.ReusableGameFramework.Traps.MoveRadnomly 
+{
+
+    // this script randomly moves the connected object in a random direction
+    public class RandomForce : MonoBehaviour
+    {
+
+        private int randomNumber; // used for a condition based on the number picked
+
+        [SerializeField] private float objectPower; // power the object will move (reccomended 10-75)
+        [SerializeField] private float forceIntervalInSeconds; // time between each random force (reccomended 1-20s)
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartCoroutine(RandomForceMove());
+        }
+
+        // repeated random force
+        #region Random Forces
+        private IEnumerator RandomForceMove() // used to randomly move objects repeatedly
+        {
+
+
+
+            randomNumber = Random.Range(1, 3); // 1 or 2 random
+
+            if (randomNumber == 1)
+            {
+                Vector2 direction1 = new Vector2(-10, 0);
+                GetComponent<Rigidbody2D>().AddForce(direction1 * objectPower);
+            }
+
+            if (randomNumber == 2)
+            {
+                Vector2 direction2 = new Vector2(10, 0);
+                GetComponent<Rigidbody2D>().AddForce(direction2 * objectPower);
+            }
+            
+            // Debug.Log("Applied Force");
+
+            yield return new WaitForSeconds(forceIntervalInSeconds);
+
+            StartCoroutine(RandomForceMove());
+
+        }
+        #endregion
+    }
+}
+
