e89f586472cc70025a374aded6afb7e28ee77642
TaylahFrederickSAE <1032576@student.sae.edu.au>
2024-08-07 10:57:47 +1000

    Revert "Created Item Pickup Script, added player character & movement."
    This reverts commit 797eeb6b297e15868d667b81ec0e634cee070eca.

--- Assets/Scripts/Item Pickups/ItemPickup.cs
+++ Assets/Scripts/Item Pickups/ItemPickup.cs
@@ -2,24 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace GAD176.ItemPickups
-    {
 public class ItemPickup : MonoBehaviour
 {
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-}
-
 }
