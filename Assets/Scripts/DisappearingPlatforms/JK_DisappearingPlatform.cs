dfa3cacfc792e18a506403ee00960166b2f2d6b4
jkemeny <1033035@student.sae.edu.au>
2024-08-13 22:47:08 +1000

    Put the disappearing platform script in a namespace to ensure no conflicts

--- Assets/Scripts/DisappearingPlatforms/JK_DisappearingPlatform.cs
+++ Assets/Scripts/DisappearingPlatforms/JK_DisappearingPlatform.cs
@@ -3,54 +3,57 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class JK_DisappearingPlatform : MonoBehaviour
+namespace SAE_24T2.ReusableGameFramework.Platform.DisappearingPlatform
 {
-    private Color platformColor;
-    [SerializeField] private float fadeSpeed = 3f;
-    [SerializeField] private float fadeInDelayInSeconds = 2f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class JK_DisappearingPlatform : MonoBehaviour
     {
-        platformColor = GetComponent<SpriteRenderer>().color;
-    }
+        private Color platformColor;
+        [SerializeField] private float fadeSpeed = 3f;
+        [SerializeField] private float fadeInDelayInSeconds = 2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
+        // Start is called before the first frame update
+        void Start()
         {
-            StartCoroutine(FadePlatformInOutRoutine());
+            platformColor = GetComponent<SpriteRenderer>().color;
         }
-    }
 
-    private IEnumerator FadePlatformInOutRoutine()
-    {
-        while (platformColor.a > 0)
+        // Update is called once per frame
+        void Update()
         {
-            platformColor.a = Mathf.MoveTowards(platformColor.a, 0, Time.deltaTime * fadeSpeed);
-            GetComponent<SpriteRenderer>().color = platformColor;
+
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                StartCoroutine(FadePlatformInOutRoutine());
+            }
+        }
+
+        private IEnumerator FadePlatformInOutRoutine()
+        {
+            while (platformColor.a > 0)
+            {
+                platformColor.a = Mathf.MoveTowards(platformColor.a, 0, Time.deltaTime * fadeSpeed);
+                GetComponent<SpriteRenderer>().color = platformColor;
+                yield return null;
+            }
+
+            GetComponent<BoxCollider2D>().enabled = false;
+
+            yield return new WaitForSeconds(fadeInDelayInSeconds);
+
+            GetComponent<BoxCollider2D>().enabled = true;
+
+            while (platformColor.a < 1)
+            {
+                platformColor.a = Mathf.MoveTowards(platformColor.a, 1, Time.deltaTime * fadeSpeed);
+                GetComponent<SpriteRenderer>().color = platformColor;
+                yield return null;
+            }
+
             yield return null;
         }
-
-        GetComponent<BoxCollider2D>().enabled = false;
-
-        yield return new WaitForSeconds(fadeInDelayInSeconds);
-
-        GetComponent<BoxCollider2D>().enabled = true;
-
-        while (platformColor.a < 1)
-        {
-            platformColor.a = Mathf.MoveTowards(platformColor.a, 1, Time.deltaTime * fadeSpeed);
-            GetComponent<SpriteRenderer>().color = platformColor;
-            yield return null;
-        }
-
-        yield return null;
     }
 }
